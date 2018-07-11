992e0a1a49cb2525927480b8b18058df1a5cfdce
codeconcept <contact@codeconcept.fr>
2018-07-11 20:58:36 +0200

    creating selectors

--- src/app/repo/repo-list/repo-list.component.ts
+++ src/app/repo/repo-list/repo-list.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Load } from '../state/repo.actions';
+import { Observable } from 'rxjs';
+
+import { Repo } from '../repo';
+import { getAllRepos, getRequestDatetime } from '../state/repo.selectors';
 
 @Component({
   selector: 'app-repo-list',
@@ -8,11 +12,15 @@ import { Load } from '../state/repo.actions';
   styleUrls: ['./repo-list.component.css']
 })
 export class RepoListComponent implements OnInit {
+  repos$: Observable<Repo[]>;
+  requestDates$: Observable<Date>;
 
   constructor(private store: Store<any>) { }
 
   ngOnInit() {
     console.log('ngOnInit repo-list');
+    this.repos$ = this.store.select(getAllRepos);
+    this.requestDates$ = this.store.select(getRequestDatetime);
     this.store.dispatch(new Load());
   }
 
